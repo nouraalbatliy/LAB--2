2491347ce054378f2b0e4b2e7a98172955b9bf96
nouraalbatliy <nourah892b@gmail.com>
2023-09-04 13:39:06 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LAB--2"
+include(":app")
+ 
